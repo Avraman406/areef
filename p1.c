0334fb3cdb8b49aeaf34e7578b07ab1edd07b2d0
Avraman406 <145466686+Avraman406@users.noreply.github.com>
2025-12-14 22:37:51 +0530

    areef1

---
+++ p1.c
@@ -0,0 +1,18 @@
+/*
+Name:ATTIPATI VENKATA RAMANAIAH
+Date:
+Title:
+Sample I/P:
+Sample O/p:
+*/
+#include<stdio.h>
+int main()
+{
+    int n;
+    scanf("%d",&n);
+    for(int i=0;i<n;i++){
+	for(int j=0;j<=i;j++)
+	    printf("* ");
+	printf("\n");
+    }
+}
